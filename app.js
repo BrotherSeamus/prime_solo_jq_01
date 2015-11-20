5e6875dae9266941167b4fdcb44f7cb6c2d76263
BrotherSeamus <jamespkirwin@gmail.com>
2015-11-20 08:03:10 -0600

    attempted handlebars no bueno

--- app.js
+++ app.js
@@ -1,50 +1,71 @@
+function Employee (eNum, eFirstName, eLastName, eTitle, eSalary, eReviewScore) {
+	this.num = eNum;
+	this.firstName = eFirstName;
+	this.lastName = eLastName;
+	this.title = eTitle
+	this.salary = eSalary;
+	this.reviewScore = eReviewScore;
+};
+
+function EmployeeList () {
+	this.list = [];
+
+	var self = this;
+	this.addEmployee = function (employee) {
+			self.list.push(employee);
+	};
+
+	this.removeEmployee = function (num) {
+		for (i=0; i< self.list.length; i++) {
+			if(num == self.list[i].num){
+				self.list.splice(i, 1);
+				break;
+			};
+		};
+	};
+};
+
+
 $(function(){
 
-	$('form').on('submit', function(event) {
+	var employeeList = new EmployeeList();
+
+	var employeeTemplate = $('#employee-list').html();
+
+	var template = Handlebars.compile(employeeTemplate);
+
+
+	$('#empInfo').on('submit', function(event) {
 		try {
-			var data = $(this).serializeArray();
-			var emp = {
-				firstName: data[0].value, 
-				lastName: data[1].value, 
-				empNum: data[2].value,
-				emptitle: data[3].value, 
-				reviewScore: data[4].value, 
-				salary: data[5].value
-			};
-			var $btn = $('<button class="remove">Remove</button>')
-			var $li = $('<li>')
 
-			$('.empList').append($li);
+			var num = $('#num').val();
+			var firstName = $('#firstName').val();
+			var lastName = $('#lastName').val();
+			var title = $('#title').val();
+			var reviewScore = $('#reviewScore').val();
+			var salary = $('#salary').val();
 
-			$li.text( "Name: " + emp.firstName + " " + emp.lastName +
-				",  Employee Number: " + emp.empNum + ",  Title: " + emp.emptitle +
-				",  Review Score: " + emp.reviewScore + ",  Salary: " + emp.salary);
-			
-			$('ul').children().last().append($btn);
-			
-			$('button').on('click', function(){
-				$(this).closest("li").remove('li')
-			})
-			if (data[4].value == 1) {
-				$li.addClass('one');
-			}
-			if (data[4].value == 2) {
-				$li.addClass('two');
-			}
-			if (data[4].value == 3) {
-				$li.addClass('three');
-			}
-			if (data[4].value == 4) {
-				$li.addClass('four');
-			}
-			if (data[4].value == 5) {
-				$li.addClass('five');
-			}
-			$(this)[0].reset();
+			employeeList.addEmployee(new Employee(num, firstName, lastName, 
+				title, salary, reviewScore));
+
+			console.log(employeeList);
+
+			$('.employees').empty();
+
+			var compiledHtml = template(employeeList);
+
+			console.log(compiledHtml);		
+
+			$('.employees').append(compiledHtml);
+
 		} catch (ex) {
 			console.log('Exception:', ex);
 		} finally {
 			event.preventDefault();
 		}
+	})
+	$('.remove').on('click', function(){
+		var kill = $(this).parents('tr').data('num');
+		employeeList.remove(kill);
 	})
 });
